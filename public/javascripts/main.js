e84daf3c89371953c18443b499aecfa490d7993e
david-lomas <podypodpod@gmail.com>
2016-03-10 17:42:24 +0000

    restructure

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,22 @@
-$.get("/tweet", function(res) {
-    console.log(res); 
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function($scope, $http) {
+
+  $scope.name = "DavidLomas20";
+
+  $scope.tweet = function() {
+    $.get("/tweet", function(res) {
+      console.log(res);
+    });
+  };
+
+
+  $scope.getTwitterProfile = function() {
+    var href = 'https://api.twitter.com/1.1/account/settings.json';
+    $http.get(href).success(function(data) {
+      console.log($scope.name);
+      console.log(data);
+
+    });
+  };
+
 });
